2b8f4f7c6dc7ee2f62ec74da92ae5af5a826948e
Kai <kai@7mind.io>
2020-04-13 15:21:22 +0100

    remove duplicate tests

--- izumi-reflect/izumi-reflect/src/test/scala/izumi/reflect/test/LightTypeTagTest.scala
+++ izumi-reflect/izumi-reflect/src/test/scala/izumi/reflect/test/LightTypeTagTest.scala
@@ -73,6 +73,10 @@ class LightTypeTagTest extends TagAssertions {
 
     }
 
+    "support self-intersection (X with X)" in {
+      assertSame(`LTT`[String with String], `LTT`[String])
+    }
+
     "support subtype checks" in {
       assertChild(LTT[Int], LTT[AnyVal])
       assertChild(LTT[Int], LTT[Int])
@@ -379,52 +383,6 @@ class LightTypeTagTest extends TagAssertions {
 
       val res = tag.combine(`LTT[_,_]`[IO])
       assert(res == LTT[BlockingIO[IO]])
-    }
-
-    "progression test: can't support subtyping of type prefixes" in {
-      val a = new C {}
-
-      intercept[TestFailedException] {
-        assertChild(LTT[a.A], LTT[C#A])
-      }
-    }
-
-    "progression test: can't support subtyping of concrete type projections" in {
-      trait A {
-
-        trait T
-
-      }
-      trait B extends A
-
-      val tagA = LTT[A#T]
-      val tagB = LTT[B#T]
-      assertSame(LTT[A#T], LTT[A#T])
-      assertDifferent(LTT[B#T], LTT[A#T])
-      intercept[TestFailedException] {
-        assertChild(tagB, tagA)
-      }
-    }
-
-    "support self-intersection (X with X)" in {
-      assertSame(`LTT`[String with String], `LTT`[String])
-    }
-
-    "progression test: wildcards are not supported" in {
-      intercept[TestFailedException] {
-        assertChild(LTT[Set[Int]], LTT[Set[_]])
-      }
-    }
-
-    "progression test: subtype check fails when child type has absorbed a covariant type parameter of the supertype" in {
-      assertChild(LTT[Set[Int]], LTT[Iterable[AnyVal]])
-
-      val tagF3 = LTT[F3]
-      assertChild(tagF3, LTT[F2[Int]])
-
-      intercept[TestFailedException] {
-        assertChild(tagF3, LTT[F2[AnyVal]])
-      }
     }
 
   }
