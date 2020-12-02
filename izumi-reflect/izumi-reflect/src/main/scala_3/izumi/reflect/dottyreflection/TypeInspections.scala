f9b64a43c5013572664fe2d7fb29f59df27f0517
scalavision <scalavision@gmail.com>
2020-12-02 17:03:32 +0100

    update scala 3 to scala 3.0.0-M2

--- izumi-reflect/izumi-reflect/src/main/scala_3/izumi/reflect/dottyreflection/TypeInspections.scala
+++ izumi-reflect/izumi-reflect/src/main/scala_3/izumi/reflect/dottyreflection/TypeInspections.scala
@@ -3,18 +3,18 @@ package izumi.reflect.dottyreflection
 import izumi.reflect.macrortti.LightTypeTag.ParsedLightTypeTag.SubtypeDBs
 import izumi.reflect.macrortti.LightTypeTagRef._
 
-import scala.quoted.{QuoteContext, Type}
+import scala.quoted.{Quotes, Type}
 
 object TypeInspections {
-  def apply[T <: AnyKind : Type](using qctx0: QuoteContext): AbstractReference = {
+  def apply[T <: AnyKind : Type](using qctx0: Quotes): AbstractReference = {
     new Inspector(0) { val qctx = qctx0 }.buildTypeRef[T]
   }
 
-  def nameDb[T <: AnyKind : Type](using qctx0: QuoteContext): Map[NameReference, Set[NameReference]] = {
+  def nameDb[T <: AnyKind : Type](using qctx0: Quotes): Map[NameReference, Set[NameReference]] = {
     new DbInspector(0) { val qctx = qctx0 }.buildNameDb[T]
   }
 
-  def fullDb[T <: AnyKind : Type](using qctx0: QuoteContext): Map[AbstractReference, Set[AbstractReference]] = {
+  def fullDb[T <: AnyKind : Type](using qctx0: Quotes): Map[AbstractReference, Set[AbstractReference]] = {
     new FullDbInspector(0) { val qctx = qctx0 }.buildFullDb[T]
   }
 }
