1032a8e75552e69829a52ea23db27d1ae9f92ba9
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-04-29 13:08:36 +0200

    Update sbt-dotty to 0.5.5 (#164)

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -24,6 +24,6 @@ object PV {
   val scala_native_version = "0.4.0"
   val crossproject_version = "1.0.0"
   val scalajs_bundler_version = "0.18.0"
-  val sbt_dotty_version = "0.5.3"
+  val sbt_dotty_version = "0.5.5"
   val sbt_mima_version = "0.8.1"
 }
