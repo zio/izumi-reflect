6d699e62db68832851a5deff686f243c533ecc1b
Kai <kai@7mind.io>
2021-07-23 12:57:18 +0100

    Update Scala.js to 1.6.0

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -20,7 +20,7 @@ object PV {
   val sbt_scoverage = "1.8.2"
   val sbt_pgp = "2.1.1"
 
-  val scala_js_version = "1.5.0"
+  val scala_js_version = "1.6.0"
   val scala_native_version = "0.4.0"
   val crossproject_version = "1.0.0"
   val scalajs_bundler_version = "0.18.0"
