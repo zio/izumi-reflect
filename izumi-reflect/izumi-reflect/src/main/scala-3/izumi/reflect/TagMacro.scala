e390f6ade404b03d7ffd364c4e8efba98980566d
Kai <450507+neko-kai@users.noreply.github.com>
2022-06-30 16:11:56 +0100

    Fix tag combining on Scala 3 to work in more cases, e.g. when type constructor is a type parameter

--- izumi-reflect/izumi-reflect/src/main/scala-3/izumi/reflect/TagMacro.scala
+++ izumi-reflect/izumi-reflect/src/main/scala-3/izumi/reflect/TagMacro.scala
@@ -18,62 +18,73 @@ final class TagMacro(using override val qctx: Quotes) extends InspectorBase {
   def createTagExpr[A <: AnyKind: Type]: Expr[Tag[A]] = {
     val typeRepr = TypeRepr.of[A].dealias
     if (allPartsStrong(typeRepr)) {
-      // NB: this should always work, but currently causes `TestModel$::ApplePaymentProvider is not a type lambda, it cannot be parameterized` test failure
-      // There's probably a missing case where a higher-kinded type is not a type lambda, with splicing inbetween causing fixup by the compiler
-      //   val ltt = Inspect.inspectAny[A]
-      val ltt = '{ Inspect.inspect[A] }
-      '{ Tag[A](classOf[Any], $ltt) }
+      createTag(typeRepr.asType)
     } else {
       summonCombinedTag[A](typeRepr)
     }
   }
 
-  private def mkLtt(typeRepr: TypeRepr): Expr[LightTypeTag] =
-    if (allPartsStrong(typeRepr))
-      typeRepr.asType match {
-//        case given Type[a] => Inspect.inspectAny[a]
-        case given Type[a] => '{ Inspect.inspect[a] }
+  private def createTag[A <: AnyKind: Type, B <: AnyKind]: Expr[Tag[B]] = {
+    // NB: this should always work, but currently causes `TestModel$::ApplePaymentProvider is not a type lambda, it cannot be parameterized` test failure
+    // There's probably a missing case where a higher-kinded type is not a type lambda, with splicing inbetween causing fixup by the compiler
+    //   val ltt = Inspect.inspectAny[A]
+    val ltt = '{ Inspect.inspect[A] }
+    '{ Tag[A](classOf[Any], $ltt) }.asInstanceOf[Expr[Tag[B]]]
+  }
+
+  private def summonCombinedTag[T <: AnyKind: Type](typeRepr: TypeRepr): Expr[Tag[T]] = {
+    def summonLttIfTypeParam(typeRepr: TypeRepr): Expr[LightTypeTag] = {
+      if (allPartsStrong(typeRepr)) {
+        typeRepr.asType match {
+  //        case given Type[a] => Inspect.inspectAny[a]
+          case given Type[a] => '{ Inspect.inspect[a] }
+        }
+      } else {
+        val result = summonTag(typeRepr)
+        '{ $result.tag }
       }
-    else {
-      val result = summonCombinedTag(typeRepr)
-      '{ $result.tag }
     }
 
-  private def summonCombinedTag[T <: AnyKind: Type](typeRepr: TypeRepr): Expr[Tag[T]] =
+    def summonTagIfTypeParam[A](typeRepr: TypeRepr): Expr[Tag[A]] = {
+      if (allPartsStrong(typeRepr)) {
+        createTag(typeRepr.asType)
+      } else {
+        summonTag(typeRepr)
+      }
+    }
+
     typeRepr.dealias match {
-      case x @ TypeRef(ThisType(_), _) if x.typeSymbol.isAbstractType && !x.typeSymbol.isClassDef => summonTag(x)
+      case x @ TypeRef(ThisType(_), _) if x.typeSymbol.isAbstractType && !x.typeSymbol.isClassDef =>
+        summonTag(x)
+
+// FIXME type lambda support pending
+//      case TypeLambda(_, _, body) =>
+//        summonTag(body)
 
       case AppliedType(ctor, args) =>
-        val ctorTag = createTagExpr(ctor.asType)
-        val argsTags = Expr.ofList(args.map(mkLtt))
+        val ctorTag = summonTagIfTypeParam(ctor)
+        val argsTags = Expr.ofList(args.map(summonLttIfTypeParam))
         '{ Tag.appliedTag[T]($ctorTag, $argsTags) }
 
       case andType: AndType =>
-        val ltts0: List[Expr[LightTypeTag]] = flattenAnd(andType).map(mkLtt)
-        val ltts: Expr[List[LightTypeTag]] = Expr.ofList(ltts0)
-        andType.asType match {
-          case given Type[a] =>
-//            val structLtt = Inspect.inspectAny[a]
-            val structLtt = '{ Inspect.inspect[a] }
-            '{ Tag.refinedTag[T](classOf[Any], $ltts, $structLtt, Map.empty) }
-        }
+        val ltts: Expr[List[LightTypeTag]] = Expr.ofList(flattenAnd(andType).map(summonLttIfTypeParam))
+        val structLtt = summonLttIfTypeParam(andType)
+        '{ Tag.refinedTag[T](classOf[Any], $ltts, $structLtt, Map.empty) }
 
       case orType: OrType =>
-        val ltts0: List[Expr[LightTypeTag]] = flattenOr(orType).map(mkLtt)
-        val ltts: Expr[List[LightTypeTag]] = Expr.ofList(ltts0)
+        val ltts: Expr[List[LightTypeTag]] = Expr.ofList(flattenOr(orType).map(summonLttIfTypeParam))
         '{ Tag.unionTag[T](classOf[Any], $ltts) }
-
-      case x if x.typeSymbol.isTypeParam => summonTag(x)
 
       case _ =>
         throw new Exception(s"Unsupported type: $typeRepr")
     }
+  }
 
-  private def summonTag[any <: AnyKind](typeRepr: TypeRepr): Expr[Tag[any]] =
+  private def summonTag[A <: AnyKind](typeRepr: TypeRepr): Expr[Tag[A]] =
     typeRepr.asType match {
       case given Type[a] =>
         val message = s"Cannot find implicit Tag[${Type.show[a]}]"
-        Expr.summon[Tag[a]].getOrElse(report.errorAndAbort(message)).asInstanceOf[Expr[Tag[any]]]
+        Expr.summon[Tag[a]].getOrElse(report.errorAndAbort(message)).asInstanceOf[Expr[Tag[A]]]
     }
 
   /**
