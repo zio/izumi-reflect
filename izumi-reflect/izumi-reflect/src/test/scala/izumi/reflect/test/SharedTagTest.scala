7ff03daf2a09b101cd203c3fe0522d8517b2f20d
Kai <450507+neko-kai@users.noreply.github.com>
2023-04-24 21:51:26 +0100

    fix 2.11 build

--- izumi-reflect/izumi-reflect/src/test/scala/izumi/reflect/test/SharedTagTest.scala
+++ izumi-reflect/izumi-reflect/src/test/scala/izumi/reflect/test/SharedTagTest.scala
@@ -966,9 +966,9 @@ abstract class SharedTagTest extends AnyWordSpec with XY[String] with TagAsserti
     }
 
     "regression test: do not be confused by a type alias of Set of an abstract type referred via this-prefix on Scala 3" in {
-      val t1 = Tag[RoleDep.RoleDeps["x", Int]].tag
-      val t2 = Tag[Set[RoleDep.RoleDep["x", Int]]].tag
-      val t3 = LTT[RoleDep.RoleDeps["x", Int]]
+      val t1 = Tag[RoleDep.RoleDeps[Int, Int]].tag
+      val t2 = Tag[Set[RoleDep.RoleDep[Int, Int]]].tag
+      val t3 = LTT[RoleDep.RoleDeps[Int, Int]]
 
       assertSameStrict(t1, t2)
       assertSameStrict(t1, t3)
