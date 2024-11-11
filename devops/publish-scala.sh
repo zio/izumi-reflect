31a0eaa77a20590323745282ccad831e361b76ee
Pavel Shirshov <pshirshov@eml.cc>
2024-11-11 19:47:03 +0000

    #493 wip

--- devops/publish-scala.sh
+++ devops/publish-scala.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 source ./devops/.env.sh
-source ./devops/.validate-publishing.sh
+#source ./devops/.validate-publishing.sh
 
 [[ -z "$SONATYPE_USERNAME" ]] && echo "Missing SONATYPE_USERNAME" && exit 1
 [[ -z "$SONATYPE_PASSWORD" ]] && echo "Missing SONATYPE_PASSWORD" && exit 1
