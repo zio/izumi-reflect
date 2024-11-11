de8a5afff7fac7b0988faf3a2780e6b5a022244c
Pavel Shirshov <pshirshov@eml.cc>
2024-11-11 18:43:55 +0000

    #493 wip

--- devops/publish-ziodocs.sh
+++ devops/publish-ziodocs.sh
@@ -14,6 +14,9 @@ awk '/<!--- docs:start --->/,/<!--- docs:end --->/' README.md >> docs/index.md
 sed -i '/<!--- docs:start --->/d' docs/index.md
 sed -i '/<!--- docs:end --->/d' docs/index.md
 
+echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" > ~/.npmrc
+npm whoami
+
 sbt -batch -no-colors -v \
     --java-home "$JAVA_HOME" \
     docs/publishToNpm
