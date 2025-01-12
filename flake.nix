9cc8d00f61fe3b3ec72dc81f4de0e091e16ff736
Pavel Shirshov <pshirshov@gmail.com>
2025-01-12 18:38:03 +0000

    direnv

--- flake.nix
+++ flake.nix
@@ -35,6 +35,11 @@
             export JDK11=${pkgs.jdk11_headless}
             export JDK17=${pkgs.jdk17_headless}
             export JDK21=${pkgs.jdk21_headless}
+            export JDK_DEV=${pkgs.graalvm-ce}
+
+            rm -f ./.env || true
+            mkdir -p ./.env
+            ln -s ''${JDK_DEV} ./.env/jdk
           '';
         };
       }
