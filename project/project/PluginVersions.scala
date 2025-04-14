379ac7675aa90eeede73466fefe4e15a95e270f6
Kai <450507+neko-kai@users.noreply.github.com>
2025-04-14 17:12:49 +0100

    Revert "Update sbt-mima-plugin to 1.1.4"
    This reverts commit 015dec82de6c75b9a20402e1bdcc27b844f5e646.

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -20,7 +20,7 @@ object PV {
   val sbt_scoverage = "2.3.1"
   val sbt_pgp = "2.3.1"
 
-  val sbt_mima_version = "1.1.4"
+  val sbt_mima_version = "1.1.0"
 
   // last 2.11 version is 1.12.0, so we excluded 2.11 from JS builds, see nscplugin on maven central
   val scala_js_version = "1.17.0"
