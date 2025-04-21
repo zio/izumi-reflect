9ac598ca588e0400826a868a49d4cf2a378b64d0
Pavel Shirshov <pshirshov@eml.cc>
2025-04-21 18:19:12 +0100

    wip

--- .mobala/env.sh
+++ .mobala/env.sh
@@ -3,43 +3,13 @@
 set -euo pipefail
 if [[ "${DO_VERBOSE}" == 1 ]] ; then set -x ; fi
 
-export IZREFLECT_VERSION=$(cat version.sbt | sed -r 's/.*\"(.*)\".**/\1/' | sed -E "s/SNAPSHOT/build."${CI_BUILD_UNIQ_SUFFIX}"/")
-
-export SCALA211=$(cat project/Deps.sc | grep 'val scala211 ' |  sed -r 's/.*\"(.*)\".**/\1/')
-export SCALA212=$(cat project/Deps.sc | grep 'val scala212 ' |  sed -r 's/.*\"(.*)\".**/\1/')
-export SCALA213=$(cat project/Deps.sc | grep 'val scala213 ' |  sed -r 's/.*\"(.*)\".**/\1/')
-export SCALA3=$(cat project/Deps.sc | grep 'val scala300 ' |  sed -r 's/.*\"(.*)\".**/\1/')
-
-[[ -z "${SCALA_VERSION+x}" ]] && echo "Missing SCALA_VERSION" && exit 1
-[[ -z "${JAVA_VERSION+x}" ]] && echo "Missing JAVA_VERSION" && exit 1
-
-case $SCALA_VERSION in
-  2.11) SCALA_VERSION="$SCALA211" ;;
-  2.12) SCALA_VERSION="$SCALA212" ;;
-  2.13) SCALA_VERSION="$SCALA213" ;;
-  3) SCALA_VERSION="$SCALA3" ;;
-  *) exit 1 ;;
-esac
-
-export SCALA_VERSION="$SCALA_VERSION"
-export VERSION_COMMAND="++ $SCALA_VERSION"
-
 JDK_VERSION_VAR="JDK${JAVA_VERSION}"
 export JAVA_HOME="${!JDK_VERSION_VAR}"
 export PATH=$JAVA_HOME/bin:$PATH
 
-export _JAVA_OPTIONS="
-  # JVM ignores HOME and relies on getpwuid to determine home directory
-  # That fails when we run self-hosted github agent under non-dynamic user
-  # We need that for rootless docker to work
-  -Duser.home=${HOME}
-  -Xmx4000M
-  -XX:ReservedCodeCacheSize=384M
-  -XX:NonProfiledCodeHeapSize=256M
-  -XX:MaxMetaspaceSize=1024M
-"
-
 set_jvm_options
+set_jvm_optimizations
+set_scala_sbtgen_variables
 debug_env
 
 # this script receives all the CLI args from the main script and may decide which flows should be enabled
