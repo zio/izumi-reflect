ca4de9e91aae797dbeb4fed045a5b780b2f6e4ab
Kai <450507+neko-kai@users.noreply.github.com>
2025-08-18 19:51:04 +0100

    Add progression test for https://github.com/zio/izumi-reflect/issues/548

--- izumi-reflect/izumi-reflect/src/test/scala-3/izumi/reflect/test/LightTypeTagProgressionTest.scala
+++ izumi-reflect/izumi-reflect/src/test/scala-3/izumi/reflect/test/LightTypeTagProgressionTest.scala
@@ -4,14 +4,16 @@ import izumi.reflect.macrortti._
 
 class LightTypeTagProgressionTest extends SharedLightTypeTagProgressionTest {
 
-  import TestModel._
-
   "[progression] lightweight type tag (Dotty)" should {
 
-    "fails to support polymorphic function types" in {
-//      val t1 = LTT[[A] => A => A]
-//      val t2 = LTT[[B] => B => B]
-//      assertSameStrict(t1, t2)
+    "fails to support variance for type parameters of opaque types" in {
+      object x {
+        opaque type T[+X] = X
+      }
+
+      broken {
+        assertChildStrict(LTT[x.T[Int]], LTT[x.T[AnyVal]])
+      }
     }
 
   }
