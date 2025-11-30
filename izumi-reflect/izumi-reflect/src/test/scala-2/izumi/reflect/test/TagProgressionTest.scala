acc7791eec7ad61a705f72959d6e44387847709f
Kai <450507+neko-kai@users.noreply.github.com>
2025-11-30 21:46:23 +0000

    fix false progression test

--- izumi-reflect/izumi-reflect/src/test/scala-2/izumi/reflect/test/TagProgressionTest.scala
+++ izumi-reflect/izumi-reflect/src/test/scala-2/izumi/reflect/test/TagProgressionTest.scala
@@ -1,9 +1,15 @@
 package izumi.reflect.test
 
-import izumi.reflect.Tag
 import org.scalatest.exceptions.TestFailedException
 
 class TagProgressionTest extends SharedTagProgressionTest {
+
+  trait Trait1 {
+    def dep: Dep
+  }
+  trait Trait3[T <: Dep] extends Trait1 {
+    def dep: T
+  }
 
   "[progression] Tag (Scala 2)" should {
 
@@ -12,12 +18,13 @@ class TagProgressionTest extends SharedTagProgressionTest {
         assertCompiles("""
         type `TagK<:Dep`[K[_ <: Dep]] = izumi.reflect.HKTag[ { type Arg[A <: Dep] = K[A] } ]
 
-        def t[T[_ <: Dep]: `TagK<:Dep`, A <: Dep: Tag] = Tag[T[A]]
+        def t[T[_ <: Dep]: `TagK<:Dep`, A <: Dep: izumi.reflect.Tag] = izumi.reflect.Tag[T[A]]
 
-        assert(t[Trait3, Dep].tag == Tag[Trait3[Dep]].tag)
+        assert(t[Trait3, Dep].tag == izumi.reflect.Tag[Trait3[Dep]].tag)
         """)
       }
       assert(t.message.get.contains("could not find implicit value"))
+      assert(t.message.get.contains("deriving"))
     }
 
   }
