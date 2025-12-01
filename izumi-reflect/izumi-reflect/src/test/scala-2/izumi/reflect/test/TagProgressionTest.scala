60eb3fb1644d2d346ac44a600a9ee529d2384157
Ariaj Sarkar <rajsarkar02205@gmail.com>
2025-12-01 12:44:50 +0530

    tests: resolve TagProgressionTest merge conflict and update expectations
    Merged upstream changes cleanly and preserved new behavior.

--- izumi-reflect/izumi-reflect/src/test/scala-2/izumi/reflect/test/TagProgressionTest.scala
+++ izumi-reflect/izumi-reflect/src/test/scala-2/izumi/reflect/test/TagProgressionTest.scala
@@ -1,9 +1,13 @@
 package izumi.reflect.test
 
-import izumi.reflect.Tag
-import izumi.reflect.test.TestModel._
-
 class TagProgressionTest extends SharedTagProgressionTest {
+
+  trait Trait1 {
+    def dep: Dep
+  }
+  trait Trait3[T <: Dep] extends Trait1 {
+    def dep: T
+  }
 
   "[progression] Tag (Scala 2)" should {
 
@@ -13,9 +17,9 @@ class TagProgressionTest extends SharedTagProgressionTest {
       // Previously this was a known limitation that would fail with "could not find implicit value".
       type `TagK<:Dep`[K[_ <: Dep]] = izumi.reflect.HKTag[{ type Arg[A <: Dep] = K[A] }]
 
-      def t[T[_ <: Dep]: `TagK<:Dep`, A <: Dep: Tag] = Tag[T[A]]
+      def t[T[_ <: Dep]: `TagK<:Dep`, A <: Dep: izumi.reflect.Tag] = izumi.reflect.Tag[T[A]]
 
-      assert(t[Trait3, Dep].tag == Tag[Trait3[Dep]].tag)
+      assert(t[Trait3, Dep].tag == izumi.reflect.Tag[Trait3[Dep]].tag)
     }
 
   }
