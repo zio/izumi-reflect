43746d80273265eb7a041b03bd5977bb999bce71
Kai <450507+neko-kai@users.noreply.github.com>
2025-12-01 16:26:21 +0000

    add test for Scala 3 and progression test for Scala 2 for Tag combining for higher-kinded type lambdas with inner and outer type bounds

--- izumi-reflect/izumi-reflect/src/test/scala-2/izumi/reflect/test/TagProgressionTest.scala
+++ izumi-reflect/izumi-reflect/src/test/scala-2/izumi/reflect/test/TagProgressionTest.scala
@@ -27,6 +27,24 @@ class TagProgressionTest extends SharedTagProgressionTest {
       assert(t.message.get.contains("deriving"))
     }
 
+    "progression test: Scala 2 fails to `support HKTag for higher-kinded type lambdas with inner and outer type bounds`" in {
+      val t = intercept[TestFailedException](assertCompiles("""
+      import izumi.reflect.{HKTag, Tag}
+
+      trait Trait3[T <: Dep]
+      trait TraitK3[T[x <: Dep] <: Trait3[x]]
+
+      type `TagK<:Dep`[K[_ <: Dep]] = HKTag[{ type Arg[A <: Dep] = K[A] }]
+      type `TagKT<:Dep`[K[T[x <: Dep] <: Trait3[x]]] = HKTag[{ type Arg[T[x <: Dep] <: Trait3[x]] = K[T] }]
+
+      def t[K[F[x <: Dep] <: Trait3[x]]: `TagKT<:Dep`, T[x <: Dep] <: Trait3[x]: `TagK<:Dep`] = Tag[K[T]]
+
+      assert(t[TraitK3, Trait3].tag == Tag[TraitK3[Trait3]].tag)
+      """))
+      assert(t.message.get.contains("could not find implicit value"))
+      assert(t.message.get.contains("deriving"))
+    }
+
   }
 
 }
