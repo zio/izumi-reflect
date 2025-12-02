ca4bc84349e8b0ab36202115cc776156618bf8c0
Paul S. <pshirshov@eml.cc>
2025-12-02 22:24:56 +0000

    nixpkgs 25.11 (#564)
    * nixpkgs 25.11
    
    * Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "izumi-reflect build environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/25.11";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
@@ -40,7 +40,7 @@
             export JDK11=${pkgs.jdk11_headless}
             export JDK17=${pkgs.jdk17_headless}
             export JDK21=${pkgs.jdk21_headless}
-            export JDK_DEV=${pkgs.graalvm-ce}
+            export JDK_DEV=${pkgs.graalvmPackages.graalvm-ce}
 
             # Create .env directory with JDK symlink (ignore errors if already exists)
             mkdir -p ./.env 2>/dev/null || true
